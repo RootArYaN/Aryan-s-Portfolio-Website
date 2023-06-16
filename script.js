1e6d7c47d38c4561b2f39a092140bcbaad524d0f
SudoARYAN <107423980+Soultaker911@users.noreply.github.com>
2023-06-16 10:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -149,6 +149,14 @@ let scene, camera, renderer;
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
       }
+      
+      function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      }
+      
+      window.addEventListener("resize", onWindowResize);
 
       // Function to animate the scene
       function animate() {
